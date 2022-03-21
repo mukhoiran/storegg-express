927d13ea846e7d5e4041ed62912343e3c7a93a6a
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-03-22 01:06:43 +0700

    routing - get, post, put, and delete method

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// GET method route
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
+  
+// POST method route
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage')
+})
+
+// PUT method route
+app.put('/', function (req, res) {
+  res.send('PUT request to homepage')
+})
+
+// DELETE method route
+app.delete('/', function (req, res) {
+  res.send('DELETE request to homepage')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

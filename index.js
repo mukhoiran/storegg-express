6dcf257002382399cf2bd82a6b01ffd4d7f5a55b
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-03-22 01:35:00 +0700

    middleware

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const routers = require('./routers')
 const app = express()
 const port = 1000
 
+//Middleware
+const myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
 app.use(routers)
 
 app.listen(port, () => {

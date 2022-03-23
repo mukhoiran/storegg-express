bf28909bea8477747f04bfc422dc6779d1cc4989
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-03-23 10:09:58 +0700

    crud - update and delete data

--- routers.js
+++ routers.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { ObjectId } = require('mongodb')
 const router = express.Router()
 const connection = require('./connection')
 
@@ -40,6 +41,47 @@ router.post('/users', async (req, res) => {
          res.send({message: 'Successfully added data'})
       }else{
          res.send({message: 'Failed adding data'})
+      }
+   } catch (error) {
+      res.send({message: error.message || 'internal server error'})  
+   }
+})
+
+//update data into storegg_db
+router.put('/users/:id', async (req, res) => {
+   try {
+      const { id } = req.params
+      const { name, age, status } = req.body
+      const db = connection.db('storegg_db')
+      const users = await db.collection('users').updateOne({ _id: ObjectId(id)}, {
+         $set: {
+            name,
+            age,
+            status
+         }
+      });
+      // console.log("users >> ")
+      // console.log(users)
+      if(users.acknowledged){
+         res.send({message: 'Successfully updated data'})
+      }else{
+         res.send({message: 'Failed updating data'})
+      }
+   } catch (error) {
+      res.send({message: error.message || 'internal server error'})  
+   }
+})
+
+//delete data from storegg_db
+router.delete('/users/:id', async (req, res) => {
+   try {
+      const { id } = req.params
+      const db = connection.db('storegg_db')
+      const users = await db.collection('users').deleteOne({ _id: ObjectId(id)});
+      if(users.acknowledged){
+         res.send({message: 'Successfully deleted data'})
+      }else{
+         res.send({message: 'Failed deleting data'})
       }
    } catch (error) {
       res.send({message: error.message || 'internal server error'})  
